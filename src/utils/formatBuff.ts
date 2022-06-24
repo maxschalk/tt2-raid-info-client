b661bdf52aca9c6eb641d780b55f6eb3b834a60e
Maximilian Schalk <maximilian.schalk@audi.de>
2022-06-25 00:00:38 +0200

    wip

---
+++ src/utils/formatBuff.ts
@@ -0,0 +1,43 @@
+import type { Buff } from 'src/types';
+
+export function formatBuff(buff: Buff, numCursedParts = 0) {
+	const buffName = formatBuffName(buff.bonus_type);
+
+	const isPercentage = buff.bonus_amount < 1;
+
+	if (isPercentage) {
+		const amountPercent = buff.bonus_amount * 100;
+
+		const totalCurseCalc =
+			numCursedParts > 0
+				? ` (Total: ${amountPercent * numCursedParts}%, ${numCursedParts} parts)`
+				: '';
+
+		return `${buffName} ${amountPercent}%${totalCurseCalc}`;
+	}
+
+	const totalCurseCalc =
+		numCursedParts > 0
+			? ` (Total: ${buff.bonus_amount * numCursedParts}, ${numCursedParts} parts)`
+			: '';
+
+	return `${buffName} ${buff.bonus_amount}${totalCurseCalc}`;
+}
+
+export function formatBuffName(buff: string) {
+	return removeCurseSuffix(buff)
+		.match(/[A-Z][a-z]+/g)
+		?.join(' ');
+}
+
+function removeCurseSuffix(buff: string) {
+	const curseSuffix = 'PerCurse';
+
+	const curseSuffixIndex = buff.indexOf(curseSuffix);
+
+	if (curseSuffixIndex === -1) {
+		return buff;
+	}
+
+	return buff.substring(0, curseSuffixIndex);
+}
