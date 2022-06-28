25b28ea2ba3fe203091ef50ca710c37c275f002e
riskypenguin <maximilian.schalk@audi.de>
2022-06-28 17:00:42 +0200

    fix api urls prod

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ import type { Stage } from '../types';
 
 export const BASE_URLS: { [K in Stage]: string } = {
 	dev: 'http://localhost:5000',
-	prod: 'http://http://riskypengu.in',
+	prod: 'http://riskypengu.in',
 };
 
 export const BASE_URLS_API: { [K in Stage]: string } = {
