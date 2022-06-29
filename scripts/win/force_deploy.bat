50791da4baedd9c77c95ef620e338b42e65595fc
riskypenguin <maximilian.schalk@audi.de>
2022-06-29 11:55:52 +0200

    feat enable commit message cli argument

--- scripts/win/force_deploy.bat
+++ scripts/win/force_deploy.bat
@@ -1,8 +1,14 @@
+IF [%1]==[] (
+    SET "CommitMessage=automated: :rocket: force deploy from dev"
+) ELSE (
+    SET "CommitMessage=%1"
+)
+
 git checkout dev
 
 git add .
 
-git commit -am "automated: :rocket: force deploy from dev"
+git commit -am %CommitMessage%
 
 git checkout main
 
