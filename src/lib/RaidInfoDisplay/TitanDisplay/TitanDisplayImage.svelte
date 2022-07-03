d045799c11d3e139487f7131862d581108211aac
riskypenguin <maximilian.schalk@audi.de>
2022-07-03 13:07:55 +0200

    add lemmy TitanDisplay

---
+++ src/lib/RaidInfoDisplay/TitanDisplay/TitanDisplayImage.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import lemmy from '$lib/assets/lemmy/lemmy.png';
+	import type { EnhancedTitan } from 'src/types';
+
+	// export let titanInfo: EnhancedTitan;
+</script>
+
+<div class="w-full flex justify-center">
+	<img src={lemmy} alt="Dummy Titan Visualization" />
+</div>
