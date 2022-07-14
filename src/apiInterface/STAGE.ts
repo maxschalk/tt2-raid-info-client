bf6f317ef358aafa6f6239ac7b2e3ae03241f90c
riskypenguin <maximilian.schalk@audi.de>
2022-07-14 20:53:10 +0200

    remove console.log

--- src/apiInterface/STAGE.ts
+++ src/apiInterface/STAGE.ts
@@ -3,8 +3,6 @@ import { Stage } from '../types';
 export const STAGE = getFromEnv('VITE_STAGE');
 export const API_STAGE = getFromEnv('VITE_API_STAGE');
 
-console.log(STAGE, API_STAGE);
-
 function getFromEnv(key: string): Stage {
     let stage = import.meta.env[key];
 
